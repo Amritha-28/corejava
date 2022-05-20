7791286ec390328e2b31cbd095e5d1c65fd69325
Amritha-28 <105904929+Amritha-28@users.noreply.github.com>
2022-05-20 11:58:14 +0530

    Add files via upload

---
+++ IncrDecr.java
@@ -0,0 +1,17 @@
+//prgm for increment and decrement example
+package programs;
+
+public class IncrDecr {
+
+	public static void main(String[] args) {
+		int a=1, k, i;
+		k= ++a;
+		i=a++ + ++a;
+		System.out.println("a= "+a);
+		System.out.println("k= "+k);
+		System.out.println("i= "+i);
+		
+
+	}
+
+}
